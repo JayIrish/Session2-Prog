01656529f150bead15cd79d226f0c1d33232f0a1
JayIrish <61055622+JayIrish@users.noreply.github.com>
2022-07-03 15:25:03 -0400

    Created/ fixxed chargerVols( )

--- Labo-2/src/GestionsVols.java
+++ Labo-2/src/GestionsVols.java
@@ -3,39 +3,57 @@ import java.util.ArrayList;
 
 import javax.swing.JTextArea;
 public class GestionsVols {
-	static final String FICHIER_CIEAIRRELAX = "src/donnees/Cie Air Ralex.txt";
-	
-	static ArrayList<Vol> listeVols;
+	static final String FICHIER_CIEAIRRELAX = "src/donnees/Cie Air Relax.txt";
+	static ArrayList<Vol> tabVols;
 	static BufferedReader tmpVolRead;
 	static BufferedWriter tmpVolWrite;
 	static JTextArea output;
+	public final int MAX_PLACES = 340;
 
 	
 
-	public static void genererVols() throws FileNotFoundException {
-		
+	public static void chargerVols() throws Exception {
 		try {
+			String ligne;
+			tabVols = new ArrayList<Vol>();
 			tmpVolRead = new BufferedReader(new FileReader(FICHIER_CIEAIRRELAX));
+			ligne = tmpVolRead.readLine();//Lire la premiére ligne du fichier
+			while (ligne != null) {//Si ligne == null alors ont a atteint la fin du fichier
+				int numVol = Integer.parseInt(ligne.substring(0, 5));
+				String destination = ligne.substring(6, 22);
+				destination = destination.trim();
+				int jour = Integer.parseInt(ligne.substring(27, 29));
+				int mois = Integer.parseInt(ligne.substring(30, 32));
+				int annee = Integer.parseInt(ligne.substring(33, 37));
+				int nbPassager = Integer.parseInt(ligne.substring(38));
+				tabVols.add(new Vol(numVol, destination, new Date(jour, mois, annee), nbPassager));
+				ligne = tmpVolRead.readLine();
+			}//fin while
+			
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			System.out.println("Fichier introuvable. Vérifiez le chemin et nom du fichier.");
 		}
-		 int i = 0;
-		 String ligne;
-		 String elems[];
-		 ligne = tmpVolRead.readLine();
-		 while(i < tailleTableaux && ligne != null) {
-			 elems = ligne.split(";");
-			 tabNoProd[i] = Integer.parseInt(elems[0]);
-			 tabPrixProd[i] = Double.parseDouble(elems[1]);
-			 ++i;//i=i+1;
-			 ligne = tmpVolRead.readLine();
-		 }
-		 tmpVolRead.close();
-		 return i;
+		catch (IOException e) {
+			System.out.println("Un probléme est arrivé lors de la manipulation du fichier. V�rifiez vos données.");
+		}catch (Exception e) { 
+			System.out.println("Un probléme est arrivé lors du chargement du fichier. Contactez l'administrateur.");
+		}finally {//Exécuté si erreur ou pas
+			tmpVolRead.close();
+		}
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void insererVol() {
+		
+	}
+	
+	public static Vol rechercherVol(Vol vol) {
+		Vol tmpVol = vol;
+		return tmpVol;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		chargerVols();
+		System.out.println(tabVols);
 
 	}
 
